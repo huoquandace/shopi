c2b213b024a613a326f88bc13412f3768cefe0f1
Do Van Thanh <thanhdv7454@co-well.vn>
2023-04-19 17:47:33 +0700

    up

--- src/layouts/components/Sidebar/index.js
+++ src/layouts/components/Sidebar/index.js
@@ -3,9 +3,19 @@ import styles from './Sidebar.module.scss';
 export default function Sidebar({children, className}) {
     return (
         <div className={[styles.Sidebar, className].join(' ')}>
-            <div className={[].join(' ')}>
-                
-            </div>
+            <ul className={styles.SidebarMenu}>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+                <li className={styles.SidebarMenuItem}>Lorem ipsum dolor sit amet.</li>
+            </ul>
             <div className={[].join(' ')}>
 
             </div>
